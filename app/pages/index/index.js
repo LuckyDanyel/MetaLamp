bb89d2606768c2e8d4c4a0acebc1fc028ca32fb9
luckyDanyel <tere00000000@gmail.com>
2021-08-18 16:47:06 +0300

    Coplted logic for UpdateForm on specifc page

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -11,12 +11,20 @@ import '../../blocks/form-elements/drop-downs/calendar/calendar.js';
 let getForm = document.querySelector('.find-rooms__form');
 let getFromBtn = getForm.btnSign;
 
-$(getFromBtn).click(function(){
-      let getTextInputCalendar = document.querySelector('#calendar__text-one').innerHTML;
-      let getTextInputCalendarSecond = document.querySelector('#calendar__text-one-second').innerHTML;;
-      let getTextInputGuest = document.querySelector('.drop-downs__guest-text').innerHTML;
+ $(getFromBtn).click(function(){
+      let getCaledarText = document.querySelector('#calendar__text-one').innerHTML;
+      if(isNaN(Number(getCaledarText.substring(0, 2))) == false) {
+      
+            let getTextInputCalendar = document.querySelector('#calendar__text-one').innerHTML;
+            let getTextInputCalendarSecond = document.querySelector('#calendar__text-one-second').innerHTML;;
+            let getTextInputGuest = document.querySelector('.drop-downs__guest-text').innerHTML;
 
-      let url = "second-page.html" + "?" + getTextInputCalendar.substring(0, 5) + "?" + getTextInputCalendarSecond.substring(0, 5) + "?" + getTextInputGuest;
-      window.location = url;
-})
+            let url = "second-page.html" + "?" + getTextInputCalendar.substring(0, 5) + "?" + getTextInputCalendarSecond.substring(0, 5) + "?" + getTextInputGuest;
+             window.location = url;  
+      } else {
+            window.alert("Введите дату");
+            console.log("Введите Дату!");
+      }
+      
+ })
 
--- app/pages/specific/specificLogic.js
+++ app/pages/specific/specificLogic.js
@@ -1,65 +1,110 @@
-const { get } = require("jquery");
 
 const date = new Date();
+function init () {
+    let sliceStrokeUrl = window.location.href.split("?");
+    console.log(decodeURIComponent(sliceStrokeUrl));
+    let numberRoom = document.querySelectorAll('.appart__header-left-item')[1];
+    let pricePerRoom = document.querySelectorAll('.appart__header-right-item')[0];
+    let getCalendarTextOne = document.querySelector('#calendar__text-one');
+    let getCalendarTextSecond = document.querySelector('#calendar__text-one-second');
+    let getCountDaysText = document.querySelectorAll('.appart__main-text')[0];
+    let getResultDaysText = document.querySelectorAll('.appart__main-text')[1];
+    let getTootalText = document.querySelectorAll('.appart__footer-total')[1];
+    controllerForm.Room(sliceStrokeUrl, numberRoom, pricePerRoom);
+    controllerForm.Date(sliceStrokeUrl, getCalendarTextOne, getCalendarTextSecond, pricePerRoom, getCountDaysText, getResultDaysText, getTootalText);
+    
 
-let sliceStrokeUrl = window.location.href.split("?");
-let rightNumber = sliceStrokeUrl[1];
-let isLux = sliceStrokeUrl[2]
-let priceRoom = sliceStrokeUrl[3].substring(0, 4);
+    let getGuestText = document.querySelector('.drop-downs__guest-text');
+    controllerForm.Guest(sliceStrokeUrl, getGuestText);
 
-let dateCalendar = decodeURIComponent(sliceStrokeUrl[4]).split(" - ");
-let dateFirts = dateCalendar[0];
-let dateDaysFirst = dateFirts.split(" ")[0];
-let dateMonthFirst = dateFirts.split(" ")[1];
+    let getBtnSend = document.querySelector('.calendar__buttons-item-send-one');
+    $(getBtnSend).click(controllerForm.UpdateDate.bind(this, getCalendarTextOne, getCalendarTextSecond, pricePerRoom, getCountDaysText, getResultDaysText, getTootalText));
 
-let dateSecond = dateCalendar[1];
-let dateDaysSecond = dateSecond.split(" ")[0];
-let dateMonthSecond = dateSecond.split(" ")[1];
-let masiveMonth = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"];
-
-let countGuset = decodeURIComponent(sliceStrokeUrl[5]);
-
-
-let numberOnPage = document.querySelectorAll('.appart__header-left-item')[1];
-let pricePerDays = document.querySelectorAll('.appart__header-right-item')[0];
-let getCalendarTextOne = document.querySelector('#calendar__text-one');
-let getCalendarTextSecond = document.querySelector('#calendar__text-one-second');
-let getGuestText = document.querySelector('.drop-downs__guest-text');
-
-
-numberOnPage.innerHTML = rightNumber;
-pricePerDays.innerHTML = priceRoom[0] + " " + priceRoom.substring(1, priceRoom.length) + "₽";
-getGuestText.innerHTML = countGuset;
-
-let nullingDaysFirst = "";
-let nullingDaysSecond = "";
-let nullingMonth = "";
-      if(dateDaysFirst < 10){
-          nullingDaysFirst = "0";
-      }
-      if(dateDaysSecond < 10) {
-          nullingDaysSecond = "0";
-      }
-      if(masiveMonth.indexOf(String(dateMonthSecond)) + 1 < 10){
-          nullingMonth = "0";
-      } 
-     
-
-getCalendarTextOne.innerHTML = nullingDaysFirst + dateDaysFirst + "." + nullingMonth + (Number(masiveMonth.indexOf(String(dateMonthFirst)) + 1)) + "." + date.getFullYear();
-getCalendarTextSecond.innerHTML = nullingDaysSecond + dateDaysSecond + "." + nullingMonth + (Number(masiveMonth.indexOf(String(dateMonthSecond)) + 1)) + "." + date.getFullYear();
+    let getClickBlockCalendarOne = document.querySelector('.calendar-date-one');
+    $(getClickBlockCalendarOne).click(controllerForm.UpdateDate.bind(this, getCalendarTextOne, getCalendarTextSecond, pricePerRoom, getCountDaysText, getResultDaysText, getTootalText));
 
 
 
-let getLikeAll = document.querySelectorAll('.like');
-let getLikeTextAll = document.querySelector('.like__text');
-
-
-getLikeAll.forEach(getLike => {
-    $(getLike).click(countLike)
-},)
-
-function countLike(event) {
-    //   console.log(event.currentTarget);
-    //   event.currentTarget.querySelector('.like__text').innerHTML =  Number(event.currentTarget.querySelector('.like__text').innerHTML) + 1; 
-    //   event.stopPropagation();
 }
+
+let controllerForm = {
+    Room: function(url, numberRoomDom, priceRoomDom) {
+        let rightNumber = url[1];
+        let isLux = url[2]
+        let priceRoom = decodeURIComponent(url[3]).split("undefined")[0];
+
+        viewForm.displayRoom(rightNumber, priceRoom, numberRoomDom, priceRoomDom);
+
+    },
+    Date: function(url, firstDateText, secondDateText, priceRoomDoom, CountDaysTextDom, ResultDaysTextDom, TootalTextDom) {
+
+        let dateCalendar = decodeURIComponent(url[4]).split(" - ");
+        let dateFirts = dateCalendar[0];
+        let dateDaysFirst = dateFirts.split(" ")[0];
+        let dateMonthFirst = dateFirts.split(" ")[1];
+            
+        let dateSecond = dateCalendar[1];
+        let dateDaysSecond = dateSecond.split(" ")[0];
+        let dateMonthSecond = dateSecond.split(" ")[1];
+        
+        viewForm.displayDate(firstDateText, dateDaysFirst, dateMonthFirst, secondDateText, dateDaysSecond, dateMonthSecond);
+        modelForm.count(dateDaysFirst, dateDaysSecond, priceRoomDoom, CountDaysTextDom, ResultDaysTextDom, TootalTextDom);
+    },
+    Guest: function(url, textblock) {
+        let countGuset = decodeURIComponent(url[5]);
+
+        viewForm.displayGuest(countGuset, textblock);
+    },
+    UpdateDate: function(firstDateDom, SecondDateDoom, priceRoomDoom, CountDaysTextDom, ResultDaysTextDom, TootalTextDom) {
+            firstDate = firstDateDom.innerHTML.substring(0, 2);
+            secondDate = SecondDateDoom.innerHTML.substring(0, 2);
+            modelForm.count(firstDate, secondDate, priceRoomDoom, CountDaysTextDom, ResultDaysTextDom, TootalTextDom);
+    },
+}
+
+let modelForm = {
+    count: function(firstDate, SecondDate, priceRoomDoom, CountDaysTextDom, ResultDaysTextDom, TootalTextDom) {
+        let resultDays = Number(priceRoomDoom.innerHTML.split("₽")[0].replace(/\s+/g, '')) * Number(SecondDate - firstDate);
+        let countDays = String(priceRoomDoom.innerHTML) + " x " + String(SecondDate - firstDate);
+        console.log();
+        viewForm.displayCount(CountDaysTextDom, countDays, resultDays, ResultDaysTextDom, TootalTextDom);
+    }
+}
+
+let viewForm = {
+    displayRoom: function(number, price, numberRoomDom, priceRoomDom) {
+        numberRoomDom.innerHTML = number;
+        priceRoomDom.innerHTML = price[0] + price.substring(1, price.length);
+    },
+    displayDate: function(firstDateText, dateDaysFirst, dateMonthFirst, secondDateText, dateDaysSecond, dateMonthSecond) {
+        let masiveMonth = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"];
+        let nullingDaysFirst = "";
+        let nullingDaysSecond = "";
+        let nullingMonth = "";
+
+              if(dateDaysFirst < 9){
+                  nullingDaysFirst = "0";
+              }
+              if(dateDaysSecond < 9) {
+                  nullingDaysSecond = "0";
+              }
+              if(masiveMonth.indexOf(String(dateMonthSecond)) < 9){
+                  nullingMonth = "0";
+              } 
+
+        firstDateText.innerHTML = nullingDaysFirst + dateDaysFirst + "." + nullingMonth + (Number(masiveMonth.indexOf(String(dateMonthFirst)) + 1)) + "." + date.getFullYear();
+        secondDateText.innerHTML = nullingDaysSecond + dateDaysSecond + "." + nullingMonth + (Number(masiveMonth.indexOf(String(dateMonthSecond)) + 1)) + "." + date.getFullYear();
+    },
+    displayGuest: function(item, textBlock){
+        textBlock.innerHTML = item;
+    },
+    displayCount: function(textBlockCount, countDays, resultDays, textBlockResult, totalBlock) {
+        textBlockCount.innerHTML = countDays + " " + "cуток";
+        textBlockResult.innerHTML = resultDays + "₽";
+        totalBlock.innerHTML = resultDays + "₽";
+    },
+}
+
+window.onload = init();
+
+
