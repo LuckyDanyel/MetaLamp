60cefb20ba3f890bf86a15457a5ad649c959ec96
luckyDanyel <tere00000000@gmail.com>
2021-08-26 20:46:57 +0300

    Styles for burger menu

--- app/blocks/header/header.js
+++ app/blocks/header/header.js
@@ -6,9 +6,11 @@ let getMenu = document.querySelector('.header__menu');
 let firstClick = false;
 function hideMenu(){
       if(firstClick == true) {
+            getBurgerMenu.classList.remove("animation__burger");
             $(getMenu).hide();
             firstClick = false;
       }else {
+            getBurgerMenu.classList.add("animation__burger");
             $(getMenu).show();
             firstClick = true;
       }
